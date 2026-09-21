06e447ff6db4e8c4de86e0db84d35056961f86b2
ryankandhaara <9stb4t6jhy@privaterelay.appleid.com>
2026-09-22 00:28:08 +0700

    Update catalog from Tunetrack Admin

--- tunetrack/assets/js/data-catalog.js
+++ tunetrack/assets/js/data-catalog.js
@@ -21,9 +21,10 @@ const products = [
       "worship",
       "freebies"
     ],
-    "image": "https://images.unsplash.com/photo-1506157786151-b8491531f063?auto=format&fit=crop&w=1000&q=85",
+    "image": "https://i.ytimg.com/vi/UzYNVXW7zQk/maxresdefault.jpg",
     "link": "pg-yangterutamasw2.html",
-    "youtube": ""
+    "youtube": "",
+    "imageCredit": "Assets of Symphony Worship"
   },
   {
     "title": "Puji dia Yesus Sukacitaku",
